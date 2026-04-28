9b8d60611ef9bff63894439dc36d5423a7934d28
jkulmus <kul18001@byui.edu>
2026-04-27 20:20:25 -0700

    intro to routing

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
+// Imports
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const app = express();
-const PORT = 3000;
-const name = process.env.NAME;
+/**
+ * Declare Important Variables
+ */
+const PORT = process.env.PORT || 3000;
 
-// fix for __dirname in ES modules
+ // Corret for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files
-app.use(express.static("public"));
+/**
+ * Setup Express Server
+ */
+const app = express();
 
-// Routes
+/**
+ * Configure Express middleware
+ */
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Declare Routes
+ */
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "src/views/home.html"));
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "about.html"));
+    res.sendFile(path.join(__dirname, "src/views/about.html"));
 });
 
-app.get("/contact", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "contact.html"));
+app.get("/products", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/views/products.html"));
 });
 
-app.get("/hello", (req, res) => {
-    res.send(`Hello, ${name}!`);
-});
-
+/**
+ * Start server
+ */
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
